5de6450a0383e306b73b183829af2b6bce0e5794
christopherpush <67174216+christopherpush@users.noreply.github.com>
2020-08-16 17:10:59 -0700

    Update test.js

--- test.js
+++ test.js
@@ -786,7 +786,7 @@ $('#yes_abuse_survey').delay(500).css('color','white');
 ];
 ///called every answer inputted for DIGFAST
      function digfast_but(digfast_ans, digfast_val) {
-       $("#back_digfast").hide(750);
+
 jQuery(function($) {
 if (digfast_count > 0) {
   $("#back_digfast").show(750);}
@@ -944,11 +944,9 @@ var ph9q_questions = [
 "Thoughts that you would be better off dead or of hurting yourself in some way"];
 function ph9q_but(ph9q_ans, typed_ans) {
 
-  ph9q_all.push(ph9q_ans);
 jQuery(function($) {
-if (digfast_count == 0) {
-                $("#back_ph9q").hide(750);}
-else {$("#back_ph9q").show(750);}})
+if (ph9q_count == 0) {
+                $("#back_ph9q").show(750)}})
 
   /// For each question in the set, if it flags as positive, add 1 to total
   ///score AND marks it to minus the answer if back button pressed after (last_an=1).
